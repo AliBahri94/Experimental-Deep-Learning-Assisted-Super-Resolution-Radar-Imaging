4ebe66687594c8a4754855ba6320dbfbae007b97
AliBahri <58846418+AliBahri94@users.noreply.github.com>
2021-03-28 16:14:20 +0430

    Update train.py

--- train.py
+++ train.py
@@ -29,6 +29,7 @@ from glob import glob
 import cv2
 import numpy as np
 import keras
+import model
 
 
 
@@ -36,7 +37,7 @@ os.chdir("./data/dataset")
 !pwd
 
 
-
+############################################ dataset
 # Model / data parameters
 # num_classes = 10
 input_shape = (256, 1, 1)
@@ -65,66 +66,12 @@ print("max: ", np.max(x_train))
 print("min: ", np.min(x_train))
 print("max: ", np.max(x_train))
 
+############################################ load model
 
-def squeeze_excite_block(input, ratio=16):
-    ''' Create a squeeze-excite block
-    Args:
-        input: input tensor
-        filters: number of output filters
-        k: width factor
-    Returns: a keras tensor
-    '''
-    init = input
-    #channel_axis = 1 if K.image_data_format() == "channels_first" else -1
-    #filters = init._keras_shape[channel_axis]
-    filters = init.shape[-1]
-    se_shape = (1, 1, filters)
+model= model.load_model()
 
-    se = GlobalAveragePooling2D()(init)
-    se = Reshape(se_shape)(se)
-    se = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
-    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
+############################################ metrics
 
-    if K.image_data_format() == 'channels_first':
-        se = Permute((3, 1, 2))(se)
-
-    x = multiply([init, se])
-    return x
-
-#Create Model 17k Parameters
-input_model= keras.layers.Input((256,1,1))
-conv_skip= Conv2D(filters=64, kernel_size=(3,1), strides=1, padding= "same", activation= "relu")(input_model)
-
-
-#Resblock 1
-conv_1= Conv2D(filters=64, kernel_size=(3,1), strides=1, padding= "same", activation= "relu")(conv_skip)
-conv_1= BatchNormalization()(conv_1)
-conv_2= Conv2D(filters=64, kernel_size=(3,1), strides=1, padding= "same", activation= "relu")(conv_1)
-conv_2= BatchNormalization()(conv_2)
-conv_2= squeeze_excite_block(conv_2, ratio=16) # Channel Attention
-conv_3= Conv2D(filters=1, kernel_size=(5,1), strides=1, padding= "same", activation= "sigmoid")(conv_skip) # Spacial Attention
-conv_4= multiply([conv_2, conv_3])
-conv= add([conv_skip,conv_4])
-conv_skip= Conv2D(filters=16, kernel_size=(3,1), strides=1, padding= "same", activation= "relu")(conv)
-
-
-#Resblock 2
-conv_1= Conv2D(filters=16, kernel_size=(3,1), strides=1, padding= "same", activation= "relu")(conv_skip)
-conv_1= BatchNormalization()(conv_1)
-conv_2= Conv2D(filters=16, kernel_size=(3,1), strides=1, padding= "same", activation= "relu")(conv_1)
-conv_2= BatchNormalization()(conv_2)
-conv_2= squeeze_excite_block(conv_2, ratio=16) # Channel Attention
-conv_3= Conv2D(filters=1, kernel_size=(5,1), strides=1, padding= "same", activation= "sigmoid")(conv_skip) # Spacial Attention
-conv_4= multiply([conv_2, conv_3])
-conv_= add([conv_skip,conv_4])
-
-final= Conv2D(filters=1, kernel_size=(3,1), strides=1, padding= "same")(conv_)
-#final= BatchNormalization()(conv)
-#final= add([input_model,conv])
-
-#final = Lambda(lambda x: (x * sd) + mean, output_shape=(256, 1, 1))(final) 
-
-model= keras.models.Model(inputs= input_model, outputs= final)
 opt = keras.optimizers.Adam(
     learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, amsgrad=False,
     name='Adam')
@@ -134,11 +81,17 @@ def metrics(y_true, y_pred):
 
 model.compile(loss="mean_squared_error", optimizer=opt, metrics= metrics)
 
-model.summary()
-plot_model(model, "/content/drive/MyDrive/Mostafa/Callback/CNN_30k_With_batch_Final_without_add3.pdf")
 
+############################################ checkpoint
 
+callback_model= keras.callbacks.ModelCheckpoint("./checkpoint/model.h5", monitor= "val_loss", verbose= 1, save_best_only= True, mode= "min")
+callback_CSV= keras.callbacks.CSVLogger("./checkpointmodel.log", append=True)
 
+############################################ training
+
+batch_size = 64
+epochs = 100
+history= model.fit(x_train, y_train_noise, batch_size= batch_size, epochs= epochs, validation_data=(x_valid, y_valid), callbacks=[callback_model, callback_CSV], shuffle= True)
 
 
 
